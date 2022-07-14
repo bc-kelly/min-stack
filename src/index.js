684a0b4446fb99fe7db88e1bb88a4c4b536e6dca
bridget <97080052+bc-kelly@users.noreply.github.com>
2022-07-14 14:50:56 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,68 @@
+// Exercise
+// Create a stack that returns the minimum element
+// in constant time
+
+function last(stack) {
+  return stack[stack.length - 1];
+}
+
+class MinStack {
+  constructor() {
+    this.stack = [];
+    this.minStack = [];
+  }
+
+  push(x) {
+    if (this.minStack.length === 0 || x <= last(this.minStack)) {
+      this.minStack.push(x);
+    }
+    this.stack.push(x);
+  }
+
+  pop() {
+    if (last(this.minStack) === last(this.stack)) {
+      this.minStack.pop();
+    }
+    return this.stack.pop();
+  }
+
+  top() {
+    return last(this.stack);
+  }
+
+  getMin() {
+    return last(this.minStack);
+  }
+}
+
+// function last(stack){
+//   return stack[stack.length - 1]
+// }
+// class MinStack {
+//   constructor(){
+//       this.stack = []
+//       this.minStack = []
+//   }
+// push(x){
+//       if(this.minStack.length === 0 || x <= last(this.minStack)){
+//           this.minStack.push(x)
+//       }
+//       this.stack.push(x)
+//   }
+
+//   pop(){
+//       if(last(this.minStack) === last(this.stack)){
+//           this.minStack.pop()
+//       }
+//       return this.stack.pop()
+//   }
+
+//   top(){
+//       return last(this.stack)
+//   }
+
+//   getMin(){
+//       return last(this.minStack)
+//   }
+
+// }
